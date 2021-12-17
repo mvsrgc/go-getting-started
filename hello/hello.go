568c8614fe589890d10d64d3107d13762369817d
mvsrgc <14826113+mvsrgc@users.noreply.github.com>
2021-12-17 13:54:35 -0500

    feat: greetings for multiple people

--- hello/hello.go
+++ hello/hello.go
@@ -10,12 +10,13 @@ func main() {
 	log.SetPrefix("greetings: ")
 	log.SetFlags(0)
 
-	message, err := greetings.Hello("Gladys")
+	names := []string{"Gladys", "Samantha", "Darrin"}
+
+	messages, err := greetings.Hellos(names)
 
 	if err != nil {
 		log.Fatal(err)
 	}
 
-	log.Print(message)
-	fmt.Println(message)
+	fmt.Println(messages)
 }

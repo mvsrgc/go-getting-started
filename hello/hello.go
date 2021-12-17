b6aebdb22b61eeefd74dfdd6a47d6a482f455cda
mvsrgc <14826113+mvsrgc@users.noreply.github.com>
2021-12-17 13:42:17 -0500

    feat: return a random greeting

--- hello/hello.go
+++ hello/hello.go
@@ -10,11 +10,12 @@ func main() {
 	log.SetPrefix("greetings: ")
 	log.SetFlags(0)
 
-	message, err := greetings.Hello("")
+	message, err := greetings.Hello("Gladys")
 
 	if err != nil {
 		log.Fatal(err)
 	}
 
+	log.Print(message)
 	fmt.Println(message)
 }
